897e786580b517eb65c63f4a3ace7855b9a938a8
Masaki Yasutake <my17560@gmail.com>
2019-09-02 17:02:23 +0900

    Modified comments.

--- conf/v1/settings.php
+++ conf/v1/settings.php
@@ -1,19 +1,28 @@
 <?php
 return [
 
-	// Options
+	// -------------------------------------------------------------------------
+	//	Options
+	// -------------------------------------------------------------------------
+
     "options" => [
         "showErrors" => true,
 		"rootDir" => __DIR__ . "/../../",
 		"sitesDir" => __DIR__ . "/../../sites/v1/",
     ],
 
-	// Loader
+	// -------------------------------------------------------------------------
+	//	Loader
+	// -------------------------------------------------------------------------
+
 	"loader" => [
 		"class" => "\Bitsmist\\v1\Plugins\Loader\DefaultLoader",
 	],
 
-	// Router
+	// -------------------------------------------------------------------------
+	//	Router
+	// -------------------------------------------------------------------------
+
 	"router" => [
 		"class" => "",
 		"routes" => [
@@ -33,14 +42,148 @@ return [
 		],
 	],
 
-	// Request
+	// -------------------------------------------------------------------------
+	//	Request
+	// -------------------------------------------------------------------------
+
 	"request" => [
 		"class" => "\Zend\Diactoros\ServerRequestFactory",
 	],
 
-	// Response
+	// -------------------------------------------------------------------------
+	//	Response
+	// -------------------------------------------------------------------------
+
 	"response" => [
 		"class" => "\Zend\Diactoros\Response",
+	],
+
+	// -------------------------------------------------------------------------
+	//	Controllers
+	// -------------------------------------------------------------------------
+
+	"controllers" => [
+		"eventController" => [
+			"class" => "Bitsmist\\v1\Plugins\Controller\EventController",
+		],
+	],
+
+	// -------------------------------------------------------------------------
+	//	Middlewares
+	// -------------------------------------------------------------------------
+
+	"middlewares" => [
+		// Session
+
+		"phpSession" => [
+			"class" => "Bitsmist\\v1\Middlewares\Session\PhpSession",
+		],
+
+		// Connection
+
+		"connectDb" => [
+			"class" => "Bitsmist\\v1\Middlewares\Connection\ConnectDb",
+		],
+
+		"disconnectDb" => [
+			"class" => "Bitsmist\\v1\Middlewares\Connection\DisconnectDb",
+		],
+
+		// Security
+
+		"headerSecurity" => [
+			"class" => "Bitsmist\\v1\Middlewares\Security\HeaderSecurity",
+		],
+
+		"specSecurity" => [
+			"class" => "Bitsmist\\v1\Middlewares\Security\SpecSecurity",
+		],
+
+		"whitelistSecurity" => [
+			"class" => "Bitsmist\\v1\Middlewares\Security\WhitelistSecurity",
+		],
+
+		// Authorizer
+
+		"sessionAuthorizor" => [
+			"class" => "Bitsmist\\v1\Middlewares\Authorizer\SessionAuthorizer",
+		],
+
+		// Formatter
+
+		"queryFormatter"	=> [
+			"class" => "Bitsmist\\v1\Middlewares\Formatter\QueryFormatter",
+		],
+
+		"dataFormatter"	=> [
+			"class" => "Bitsmist\\v1\Middlewares\Formatter\DataFormatter",
+		],
+
+		// Renderer
+
+		"jsonRenderer"	=> [
+			"class" => "Bitsmist\\v1\Middlewares\Renderer\JsonRenderer",
+		],
+
+		// Header builder
+
+		"extraHeader"	=> [
+			"class" => "Bitsmist\\v1\Middlewares\Header\ExtraHeader",
+		],
+
+		"originHeader"	=> [
+			"class" => "Bitsmist\\v1\Middlewares\Header\OriginHeader",
+		],
+
+		// Handler
+
+		"autoHandler"	=> [
+			"class" => "Bitsmist\\v1\Middlewares\Handler\AutoHandler",
+		],
+
+		"modelHandler"	=> [
+			"class" => "Bitsmist\\v1\Middlewares\Handler\ModelHandler",
+		],
+
+		"customHandler"	=> [
+			"class" => "Bitsmist\\v1\Middlewares\Handler\CustomHandler",
+		],
+
+		"paginationHandler"	=> [
+			"class" => "Bitsmist\\v1\Middlewares\Handler\PaginationHandler",
+		],
+
+		"resultHandler"	=> [
+			"class" => "Bitsmist\\v1\Middlewares\Handler\ResultHandler",
+		],
+
+		"statuscodeHandler"	=> [
+			"class" => "Bitsmist\\v1\Middlewares\Handler\StatuscodeHandler",
+		],
+
+		// Exception handler
+
+		"basicExceptionHandler" => [
+			"class" => "Bitsmist\\v1\Middlewares\Exception\BasicExceptionHandler",
+		],
+
+		"loggingExceptionHandler" => [
+			"class" => "Bitsmist\\v1\Middlewares\Exception\LoggingExceptionHandler",
+		],
+
+		"echoExceptionHandler"	=> [
+			"class" => "Bitsmist\\v1\Middlewares\Exception\EchoExceptionHandler",
+		],
+	],
+
+	// -------------------------------------------------------------------------
+	//	Emitters
+	// -------------------------------------------------------------------------
+
+	"emitters" => [
+		"httpEmitter" => [
+			"class" => "\Zend\HttpHandlerRunner\Emitter\SapiEmitter",
+		],
 	],
 
 ];
